46fdae590c43ec823ecb817756711eb6746392a8
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-05-21 13:44:26 +0300

    Update editName.js

--- services/users/editName.js
+++ services/users/editName.js
@@ -4,9 +4,9 @@ const editName = async (request, response) => {
   const user = await userModel.findOne({ _id: request.id });
   if (user) {
     await userModel.updateOne({ _id: request.id },{name});
-    return response.json({ message: "User Name Updated Succefully" });
+    return response.json({status:"true", message: "User Name Updated Succefully" });
   } else {
-    return response.json({ message: "User Not Found" });
+    return response.json({status:"false", message: "User Not Found" });
   }
 };
 module.exports = editName;
