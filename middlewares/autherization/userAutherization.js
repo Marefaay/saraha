e10ea8fcb2a2a16214303e55be6a5f3afbec1f1f
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-05-21 13:51:33 +0300

    Update userAutherization.js

--- middlewares/autherization/userAutherization.js
+++ middlewares/autherization/userAutherization.js
@@ -4,18 +4,18 @@ const userAutherization = async (request, response, next) => {
   const token = request.header("token");
   jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, decoded) => {
     if (err) {
-      return response.json(err);
+      return response.json({status:"false",message:err});
     } else {
       const user =await userModel.findOne({ _id: decoded.userId });
       if (!user) {
-        return response.json({ message: "This User Not Found" });
+        return response.json({status:"false", message: "This User Not Found" });
       } else {
         console.log(user.emailConfirm)
         if (user.emailConfirm == true) {
           request.id = decoded.userId;
           next();
         } else {
-          return response.json({ message: "You Must Confirm You Email" });
+          return response.json({status:"false", message: "You Must Confirm You Email" });
         }
       }
     }
