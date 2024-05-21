7c65871b3f85e732008b3722d53a18531aa785a4
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-05-21 13:47:47 +0300

    Update logout.js

--- services/users/logout.js
+++ services/users/logout.js
@@ -3,19 +3,19 @@ const logout = async (request, response) => {
   const token = request.header("token");
 
   if (!token) {
-    return response.send("Access denied. No token provided.");
+    return response.json({status:"false",message:"Access denied. No token provided."});
   }
 
   // Check if the token is blacklisted
   if (blacklist.includes(token)) {
-    return response.send("Access denied. Token has been invalidated.");
+    return response.json({status:"false",message:"Access denied. Token has been invalidated."});
   }
 
   // Add the token to the request object for further use
   request.token = token;
   blacklist.push(request.token);
   console.log(blacklist);
-  response.send("Logged out successfully");
+  response.json({status:"true",,message:"Logged out successfully"});
 };
 
 module.exports = logout;
