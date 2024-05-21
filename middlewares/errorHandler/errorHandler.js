032f2d85da121fd82110348245a32d4f0ffd59c1
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-05-21 13:51:57 +0300

    Update errorHandler.js

--- middlewares/errorHandler/errorHandler.js
+++ middlewares/errorHandler/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, request, response, next) => {
-  response.json({
+  response.json({status:"false",
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
