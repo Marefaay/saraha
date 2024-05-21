e58e314b3effe51ca8770cb5940aaa0d26251941
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-05-21 13:50:33 +0300

    Update userAuthentication.js

--- middlewares/authentication/userAuthentication.js
+++ middlewares/authentication/userAuthentication.js
@@ -3,7 +3,7 @@ const userAuthentication = async (request, response, next) => {
   const token = request.header("token");
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
     if (err) {
-      return response.json({ message: err });
+      return response.json({status:"false", message: err });
     } else {
     }
     request.id = decoded.userId;
