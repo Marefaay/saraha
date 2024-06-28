4360195387ea57b1a997bab058d009eaa0be8936
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-28 18:04:52 +0300

    Update errorHandler.js

--- middlewares/errorHandler/errorHandler.js
+++ middlewares/errorHandler/errorHandler.js
@@ -1,7 +1,11 @@
-const errorHandler = (err, request, response)=> {
-  response.json({status:"false",
+const errorHandler = (err, request, response, next) => {
+  const statusCode = err.status || 500;
+  response.status(statusCode);
+  response.json({
+    status: "Error",
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack, // Hide stack trace in production
   });
 };
+
 module.exports = errorHandler;
