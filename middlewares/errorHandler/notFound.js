4b018dafab7b6bb41f7ee07c571b3f67bdf15a8c
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-28 18:04:29 +0300

    Update notFound.js

--- middlewares/errorHandler/notFound.js
+++ middlewares/errorHandler/notFound.js
@@ -1,5 +1,7 @@
 const notFound = (request, response, next) => {
   const error = new Error(`Not Found - ${request.originalUrl}`);
+  error.status = 404;
   next(error);
 };
+
 module.exports = notFound;
