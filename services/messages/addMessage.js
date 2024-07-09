08b9ae944a9081857177085ff799395f42becaa6
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-07-09 17:45:36 +0300

    Update addMessage.js

--- services/messages/addMessage.js
+++ services/messages/addMessage.js
@@ -1,15 +1,19 @@
 const messageModel = require("../../models/message.model");
+const userModel = require("../../models/user.model");
 // const jwt = require("jsonwebtoken");
 const addMessage = async (request, response) => {
-  const { message, userId } = request.body;
-//   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-//     if (err) {
-//       return response.json({ status:"false", message: err.message });
-//     } else {
-     
-//     }
-//   });
-  await messageModel.insertMany({ message, userId });
-  return response.json({ status:"true",message: "Messge Sended Succefully" });
+  const { message, email } = request.body;
+  const userFound = await userModel.findOne({email});
+  // console.log(userFound);
+
+  //   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+  //     if (err) {
+  //       return response.json({ message: err });
+  //     } else {
+
+  //     }
+  //   });
+  await messageModel.insertMany({ message, userId:userFound._id });
+  return response.json({ message: "Messge Sended Succefully" });
 };
 module.exports = addMessage;
